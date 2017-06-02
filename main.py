e5d87a359be73b40a4a20c940c823e7b83fdb747
maiux <andreamaioli92@gmail.com>
2017-06-02 19:54:01 +0200

    2D to pseudo-3d + error computation. No precise projection needed

--- main.py
+++ main.py
@@ -15,6 +15,30 @@ dist_coeff = np.array([[float(0.23320571), float(-0.11904307), float(0.00389023)
 # extrinsic parameters of the camera: rotation and translation matrix
 
 
+# ported from modules/calib3d/src/five-point.cpp line 373
+def computeError(p1, p2, essential_mat):
+	E = np.asmatrix(essential_mat)
+
+	# the point is in 2D, so we assume the z parameter as 1
+	# the assumption may lea to wrong projections but since we project both points with the essential matrix
+	# we don't account for any projection error (the x, y and z will have the same bias in both points)
+	x1 = np.array([[p1[0]], [p1[1]], [1.0]])
+	x2 = np.array([[p2[0]], [p2[1]], [1.0]])
+	
+	Ex1    = E * x1
+	Etx2   = E.transpose() * x2
+	x2tEx1 = x2.transpose().dot(Ex1)
+
+	a = Ex1[0] * Ex1[0]
+	b = Ex1[1] * Ex1[1]
+	c = Etx2[0] * Etx2[0]
+	d = Etx2[1] * Etx2[1]
+
+	return float(x2tEx1 * x2tEx1 / (a + b + c + d))
+
+# i = iteration number; M = number of matched features, B = block size 
+def preemptionFunction(i, M, B = 100):
+	return M * np.power(2, np.floor(-i/B))
 
 def features_detection(image, fps):
     key_points, key_points_desc = features_detection_engine.detectAndCompute(image, None)
@@ -22,19 +46,32 @@ def features_detection(image, fps):
     observations = motion_estimation.onNewFeaturesDiscovered(image, key_points, key_points_desc, fps)
     hypotheses = []
     if observations is not None:
+
+    	# hypotheses generation
+
         for i in range(0, len(observations)+1):
-        	# pass 6 points (5 points + one for validation)
+        	# pass 6 points (5 points + one for validation, so to have a 3x3 matrix instead of 3x6 / 3x9)
             rand = np.random.choice(len(observations), 6, replace=False)
+
             five_features = [observations[x] for x in rand]
+            
             f1_points = np.array([x[0].pt for x in five_features])
             f2_points = np.array([x[1].pt for x in five_features])
+            
+            # get the essential matrix with the 5 point method
             essential_mat = cv2.findEssentialMat(f1_points, f2_points, camera_matrix)
+            
             hypotheses.append(essential_mat[0])
-            R1, R2, t = cv2.decomposeEssentialMat(essential_mat[0])
-            print(f1_points[0])
-            #print(cv2.recoverPose(essential_mat[0], f1_points, f2_points, camera_matrix))
-            sys.exit(0)
+            
+            #R1, R2, t = cv2.decomposeEssentialMat(essential_mat[0])
+            #for j in range(0,5):
+            #	print(computeError(f1_points[j], f2_points[j], essential_mat[0]))
+            #sys.exit(0)
+
         print("Generated "+str(i)+" hypoteses")
+
+        # Pre-Emptive RANSAC TODO HERE
+
 
     return True
 
