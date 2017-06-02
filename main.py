0c4905dd6ea24bd048ea0721ce1a9c906902fb80
maiux <andreamaioli92@gmail.com>
2017-06-02 23:09:58 +0200

    Preemptive ransac done. Need to get the camera movement. Need to optimize data ordering.

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import numpy as np
 import cv2
 import sys
 import time
+import operator
 
 features_detection_engine = None
 
 # intrinsix parameter of the camera
 camera_matrix = np.asmatrix([[float(1.18848290e+03), 0.0, float(6.42833462e+02)],
-                 [0.0, float(1.18459614e+03), float(3.86675542e+02)],
-                 [0.0, 0.0, 1.0]])
+				 [0.0, float(1.18459614e+03), float(3.86675542e+02)],
+				 [0.0, 0.0, 1.0]])
 
 dist_coeff = np.array([[float(0.23320571), float(-0.11904307), float(0.00389023), float(0.00985417), float(0.55733118)]])
 # extrinsic parameters of the camera: rotation and translation matrix
@@ -25,7 +26,7 @@ def computeError(p1, p2, essential_mat):
 	x1 = np.array([[p1[0]], [p1[1]], [1.0]])
 	x2 = np.array([[p2[0]], [p2[1]], [1.0]])
 	
-	Ex1    = E * x1
+	Ex1	= E * x1
 	Etx2   = E.transpose() * x2
 	x2tEx1 = x2.transpose().dot(Ex1)
 
@@ -38,80 +39,125 @@ def computeError(p1, p2, essential_mat):
 
 # i = iteration number; M = number of matched features, B = block size 
 def preemptionFunction(i, M, B = 100):
-	return np.floor(M * np.power(2, np.floor(-i/B)))
+	return int(np.floor(M * np.power(2, np.floor(-i/B))))
 
 def features_detection(image, fps):
-    key_points, key_points_desc = features_detection_engine.detectAndCompute(image, None)
+	key_points, key_points_desc = features_detection_engine.detectAndCompute(image, None)
 
-    observations = motion_estimation.onNewFeaturesDiscovered(image, key_points, key_points_desc, fps)
-    hypotheses = []
-    if observations is not None:
+	observations = motion_estimation.onNewFeaturesDiscovered(image, key_points, key_points_desc, fps)
+	hypotheses = []
+	if observations is not None:
 
-    	# hypotheses generation
+		# hypotheses generation
+		#print(cv2.findEssentialMat(np.array([x[0].pt for x in observations]), np.array([x[1].pt for x in observations]), camera_matrix))
+		
+		while len(hypotheses) < len(observations):
+			# get 6 points (5 points to get the essential matrices + one for validation, so to have a sigle 3x3 matrix instead of n 3x3 matrices)
+			rand = np.random.choice(len(observations), 6, replace=False)
 
-        for i in range(0, len(observations)+1):
-        	# pass 6 points (5 points + one for validation, so to have a 3x3 matrix instead of 3x6 / 3x9)
-            rand = np.random.choice(len(observations), 6, replace=False)
+			five_features = [observations[x] for x in rand]
+			
+			f1_points = np.array([x[0].pt for x in five_features])
+			f2_points = np.array([x[1].pt for x in five_features])
+			
+			# get the essential matrix/matrices with the 5 point method
+			essential_mat = cv2.findEssentialMat(f1_points[0:5], f2_points[0:5], camera_matrix)
+			
+			if essential_mat[0] is None:
+				continue
 
-            five_features = [observations[x] for x in rand]
-            
-            f1_points = np.array([x[0].pt for x in five_features])
-            f2_points = np.array([x[1].pt for x in five_features])
-            
-            # get the essential matrix with the 5 point method
-            essential_mat = cv2.findEssentialMat(f1_points, f2_points, camera_matrix)
-            
-            hypotheses.append(essential_mat[0])
-            
-            #R1, R2, t = cv2.decomposeEssentialMat(essential_mat[0])
-            #for j in range(0,5):
-            #	print(computeError(f1_points[j], f2_points[j], essential_mat[0]))
-            #sys.exit(0)
+			mat = []
+			for j in range(0, essential_mat[0].shape[0], 3):
+				mat.append(np.array([essential_mat[0][j], essential_mat[0][j+1], essential_mat[0][j+2]]))
 
-        print("Generated "+str(i)+" hypoteses")
+			best = computeError(f1_points[5], f2_points[5], mat[0])
+			bestEMat = mat[0]
+			
+			for j in range(1, len(mat)):
+				if computeError(f1_points[5], f2_points[5], mat[j]) < best:
+					bestEMat = mat[j]
+			
+			hypotheses.append(bestEMat)
 
-        # Pre-Emptive RANSAC TODO HERE
+		#print("Generated "+str(len(hypotheses))+" hypotheses")
 
+		# Pre-Emptive RANSAC TODO HERE
 
-    return True
+		# len of hypothesis set
+		M = len(hypotheses)
+		f = preemptionFunction(1, M)
+		scores = []
+
+		# first step of preemptive ransac
+		for h in range(0, len(hypotheses)):
+			score = computeError(observations[0][0].pt, observations[0][1].pt, hypotheses[h])
+			scores.append((h, score))
+
+		lenHypotheses = len(hypotheses)
+		
+
+		# scan all observations
+		for o in range(1, len(observations)):
+			
+			# incremental update of the scores
+			for h in range(0, lenHypotheses):
+				score = computeError(observations[o][0].pt, observations[o][1].pt, hypotheses[h])
+				scores[h] = (scores[h][0], scores[h][1]+score)
+
+			# recalculate the preemption function
+			f = preemptionFunction(o+1, M)
+
+			# if only one hypothesis exit from the loop
+			if f == 1:
+				break
+
+			# need to reorder and remove hypotheses with bad score
+			if lenHypotheses > f:
+				scores = sorted(scores, key=lambda x: x[1], reverse=False)[0:f]
+				lenHypotheses = f
+
+		# the correct hypothesis
+		hypothesis = hypotheses[scores[0][0]]
+		print(hypothesis)
+	return True
 
 
 def play_video(path):
-    cap = cv2.VideoCapture(path)
+	cap = cv2.VideoCapture(path)
 
-    fps_rate = cap.get(cv2.CAP_PROP_FPS)
-    frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+	fps_rate = cap.get(cv2.CAP_PROP_FPS)
+	frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+	frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    print("Reading ", frame_width, "x", frame_height, " @", fps_rate, "fps.")
+	print("Reading ", frame_width, "x", frame_height, " @", fps_rate, "fps.")
 
-    framesConsidered = 0
-    t1 = time.time()
-    fps = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
+	framesConsidered = 0
+	t1 = time.time()
+	fps = 0
+	while cap.isOpened():
+		ret, frame = cap.read()
 
-        framesConsidered += 1
+		framesConsidered += 1
 
-        elapsed = time.time() - t1
-        if elapsed >= 1:
-            fps = framesConsidered / elapsed
-            t1 = time.time()
-            print("FPS: " + str(fps))
-            framesConsidered = 0
+		elapsed = time.time() - t1
+		if elapsed >= 1:
+			fps = framesConsidered / elapsed
+			t1 = time.time()
+			print("FPS: " + str(fps))
+			framesConsidered = 0
 
-        if ret:
-            if features_detection(frame.copy(), fps):
-                cv2.imshow('untracked_features', frame)
+		if ret:
+			if features_detection(frame.copy(), fps):
+				cv2.imshow('untracked_features', frame)
 
-        if cv2.waitKey( 1 ) & 0xFF == ord('q'):
-            break
+		if cv2.waitKey( 1 ) & 0xFF == ord('q'):
+			break
 
 
-    cap.release()
-    cv2.destroyAllWindows()
+	cap.release()
+	cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    features_detection_engine = cv2.ORB_create(nfeatures=650)
+	features_detection_engine = cv2.ORB_create(nfeatures=650)
 
-    play_video('media/test_1.mp4')
+	play_video('media/test_1.mp4')
