15c1ac69e0496e391185087492e077a8c985e120
maiux <andreamaioli92@gmail.com>
2017-06-05 23:20:20 +0200

    ransac implementation in python to compare without considering overhead of wrappers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import features_detection
 import frame_class
 import motion_estimation
+import ransac
 
 previous_frame = None
 
@@ -26,11 +27,12 @@ def start_motion_estimation(path):
         if succ: # Frame was correctly acquired
             frames_considered += 1
             time_elapsed = time.time() - time_snap
+            current_fps = float("{0:.2f}".format(frames_considered / time_elapsed))
 
-            if time_elapsed > 1:
-                current_fps = float("{0:.2f}".format(frames_considered / time_elapsed))
-                time_snap = time.time()
-                frames_considered = 0
+            #if time_elapsed > 1:
+            #    current_fps = float("{0:.2f}".format(frames_considered / time_elapsed))
+            #   time_snap = time.time()
+            #    frames_considered = 0
 
             new_frame = frame_class.Frame(frame.copy(), 0 if previous_frame is None else previous_frame.get_frame_id()+1)
             new_frame.find_key_points()
@@ -38,6 +40,8 @@ def start_motion_estimation(path):
             if previous_frame is not None:
                 matched_features = features_detection.match_features(previous_frame, new_frame, True)
                 hypothesis = motion_estimation.preemptive_ransac_motion_estimation(previous_frame, new_frame, matched_features)
+                #hypothesis = ransac.RANSAC_run(matched_features)
+                print(hypothesis)
 
             cv2.putText(frame, str(current_fps) + " FPS", (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0))
             cv2.imshow("Untracked Features", frame)
